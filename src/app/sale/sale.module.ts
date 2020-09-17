fc6e9c853d515437b96ea165b18c8b9d050d8d57
mayuri singhal <mayuri.singhal@engineerbabu.in>
2020-09-17 19:01:19 +0530

    quotation

--- src/app/sale/sale.module.ts
+++ src/app/sale/sale.module.ts
@@ -4,6 +4,10 @@ import { SaleRoutingModule } from './sale-routing.module';
 import { SaleLayoutComponent } from '../sale/sale-layout/sale-layout.component';
 import { QuotationComponent } from './pages/quotation/quotation.component';
 import { SharedModule } from '../../app/shared/shared.module';
+import { AddQuotationComponent } from './pages/add-quotation/add-quotation.component';
+import { InprogressComponent } from './pages/inprogress/inprogress.component';
+import { CompletedComponent } from './pages/completed/completed.component';
+import { RejectedComponent } from './pages/rejected/rejected.component';
 @NgModule({ 
   imports: [
     CommonModule,
@@ -11,6 +15,6 @@ import { SharedModule } from '../../app/shared/shared.module';
     SaleRoutingModule
   ],
 // tslint:disable-next-line: max-line-length
-  declarations: [SaleLayoutComponent,QuotationComponent]
+  declarations: [SaleLayoutComponent,QuotationComponent, AddQuotationComponent, InprogressComponent, CompletedComponent, RejectedComponent]
 })
 export class SaleModule { }
